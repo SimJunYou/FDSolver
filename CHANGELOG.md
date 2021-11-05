1f2829d330fbc2577ba5cc47e8de6fbb6ca6b1d6
Sim Jun You <simjunyou99@gmail.com>
2021-11-06 02:14:07 +0800

    Added versioning, a changelog, and a license

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+
+## 0.2
+
+This version implemented interactive BCNF decomposition and includes many bug fixes for Solver related functionality,
+like BCNF checking and lossless-join checking.
+Also started developing documentation and tracking changes.
+
+## 0.1
+
+This version implemented the 3 main classes, the IO classes, and the Solver class. Relevant tests were also added where.
--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
+try:
+      from setuptools import setup, find_packages
+except ImportError:
+      from distutils.core import setup
 
-from setuptools import setup, find_packages
-
-setup(name='fdsolver',
-      version='0.1',
+setup(name='FDSolver',
+      version='0.2',
       description='Solver for Functional Dependencies',
+      url='https://github.com/SimJunYou/FDSolver',
       author='Sim Jun You',
+      author_email='simjunyou99@gmail.com',
       packages=find_packages(),
-     )
+      license='MIT license'
+      ) # TODO: Fill in the rest of the stuff if necessary
